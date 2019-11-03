b52e0b253208921cf732edd0478eca146a408b01
LeoLazcano <laio_laz@hotmail.com>
2019-11-03 17:37:48 -0300

    Estructuras del formulario

---
+++ tpintegrador/frmReporte5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tpintegrador
+{
+    public partial class frmReporte5 : Form
+    {
+        public frmReporte5()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ tpintegrador/frmReporte9.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tpintegrador
+{
+    public partial class frmReporte9 : Form
+    {
+        public frmReporte9()
+        {
+            InitializeComponent();
+        }
+    }
+}
